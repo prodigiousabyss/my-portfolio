668402e1e0b279ad42c1277beab3af0b7badaf83
Pooja <poojaghanghas479@gmail.com>
2023-04-18 17:44:12 +0530

    temp fix

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { Route, Routes } from "react-router";
 import { Navbar } from "./components/Navbar";
 import { Home } from "./pages/Home";
-import { Crafts } from "./pages/Crafts/Crafts";
+// import { Crafts } from "./pages/Crafts/Crafts";
 import { ToDoList } from "./pages/ToDoList";
 import { Code } from "./pages/Code/Code";
 import "./App.css";
@@ -16,7 +16,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/designs" element={<Designs />} />
-        <Route path="/designs/crafts" element={<Crafts />} />
+        {/* <Route path="/designs/crafts" element={<Crafts />} /> */}
         <Route path="/codingProjects" element={<Code />} />
         <Route path="/codingProjects/toDoList" element={<ToDoList />} />
         {/* <Route path="*" /> */}
